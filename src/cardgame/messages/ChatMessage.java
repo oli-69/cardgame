3ad99a34af5a8ee8390acf1a20cd29f04e3fd701
Oliver <ofh3@hotmail.com>
2021-05-02 17:02:54 +0200

    Added 'beep'-property to chat message

--- src/cardgame/messages/ChatMessage.java
+++ src/cardgame/messages/ChatMessage.java
@@ -7,6 +7,7 @@ public class ChatMessage {
     public final String action = "chatMessage";
     public String text;
     public String sender;
+    public boolean beep;
 
     public ChatMessage() {
         this("");
@@ -16,11 +17,20 @@ public class ChatMessage {
         this(text, null);
     }
 
+    public ChatMessage(String text, boolean beep) {
+        this(text, null, beep);
+    }
+
     public ChatMessage(String text, Player sender) {
+        this(text, sender, false);
+    }
+
+    public ChatMessage(String text, Player sender, boolean beep) {
         this.text = text;
         if (sender != null) {
             this.sender = sender.getName();
         }
+        this.beep = beep;
     }
 
 }
